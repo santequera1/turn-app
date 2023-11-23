3464ca9e9f98a05d9697d7a72ec42fb47aa4e68e
Stiven Antequera Ferrer <90526249+santequera1@users.noreply.github.com>
2023-11-23 05:02:20 -0500

    Update urls.py
    actualizando URL de views, esa mondá no quería salir

--- TurneroProyecto/urls.py
+++ TurneroProyecto/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
-from turnos_app import views  # Importa las vistas de la aplicación turnos_app
+from turnos_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.pagina_principal, name='pagina_principal'),  # Usa el nombre correcto de la vista
-    path('agendar-turno/', include('turnos_app.urls')),
+    path('', views.pagina_principal, name='pagina_principal'),
+    path('agendar-turno/', views.agendar_turno, name='agendar_turno'),
+    path('lista-turnos/', views.lista_turnos, name='lista_turnos'),
+    path('atender-turnos/', views.atender_turnos, name='atender_turnos'),
+    path('atender-turnos2/', views.atender_turnos, name='atender_turnos2'),
+    path('reiniciar-numeros/', views.reiniciar_numeros, name='reiniciar_numeros'),
+    path('borrar-registros/', views.reiniciar_numeros_de_turno, name='borrar-registros'),
+    path('imprimir-datos/', views.imprimir_datos, name='imprimir_datos'),
+    path('descargar-csv/', views.descargar_csv, name='descargar_csv'),
+    path('enviar-correo-atencion/', views.enviar_correo_atencion, name='enviar_correo_atencion'),
+    path('confirmar-reinicio/', views.enviar_correo_atencion, name='confirmar-reinicio'),
+    path('obtener-lista-turnos/', views.obtener_lista_turnos, name='obtener_lista_turnos'),
+    path('agendar-turno/llamar-cliente/<int:turno_id>/', views.llamar_cliente, name='llamar_cliente'),
 ]
